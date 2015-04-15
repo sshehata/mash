5d27bbee05bba0e86d1a4ba0139e232ed2070c9c
Samy Shihata <samy.s.shihata@gmail.com>
2015-04-15 19:16:02 +0200

    url processing node updated to the new port design

--- src/graph.py
+++ src/graph.py
@@ -10,27 +10,32 @@ class RemoveUrls:
 
   URL_REGEX = r"(http(s)?://.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
 
-  def __init__(self, input_port, output_port):
+  def __init__(self, input_port):
     self.input_port = input_port
-    self.output_port = output_port
+    self.output_port = Port([], self.run)
 
   def run(self):
-    data = self.input_port.get()
-    records = data.records
-    recorde = [re.sub(RemoveUrls.URL_REGEX, "URL", record) for record in records]
-    self.output_port.update(data)
+    records = self.input_port.get()
+    records = [re.sub(RemoveUrls.URL_REGEX, "URL", record) for record in records]
+    self.output_port.update(records)
+
+  def get_ouput_ports(self):
+    return [self.output_port]
+
 
 class Port:
   # TODO Update the active set of flags
 
-  def __init__(self, required_flags):
+  def __init__(self, required_flags, ex_func):
     # To check compatibility between ports Ex:
     self.required_flags = required_flags
+    self.ex_func = ex_func
+    self.data = None
     #POS_TAGGED, TOKENIZED
 
   def get(self):
     if not self.data:
-      raise Exception, "Port contains no data"
+        self.ex_func()
     return self.data
 
   def update(self, data):
--- src/runme.py
+++ src/runme.py
@@ -3,15 +3,12 @@ from graph import RemoveUrls, Port
 from unigram_counter import UnigramCounter
 
 tweets = ["www.google.com", "facebook.com that controls my life www.grooveshark.com which I love."]
-data = DataObject(tweets)
-ip1 = Port([])
-op1 = Port([])
-ip1.update(data)
-node = RemoveUrls(ip1, op1)
-node.run()
-ip2 = op1
-op2 = Port([])
-node_unigram = UnigramCounter(ip2, op2)
-node_unigram.run()
+ip1 = Port([], None)
+ip1.update(tweets)
+node = RemoveUrls(ip1)
+#ip2 = op1
+#op2 = Port([])
+#node_unigram = UnigramCounter(ip2, op2)
+#node_unigram.run()
 print tweets
-print op2.data.records
+print node.get_ouput_ports()[0].get()
