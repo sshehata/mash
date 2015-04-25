543c37a700063bc8519f1c0156f37e63e6342c2e
Samy Shihata <samy.s.shihata@gmail.com>
2015-04-25 14:50:08 +0300

    changed get_output_ports to dictionaries

--- src/graph.py
+++ src/graph.py
@@ -13,21 +13,21 @@ class RemoveUrls:
 
   def __init__(self, input_port):
     self.input_port = input_port
-    self.output_port = Port([], self.run)
+    self.output_ports = {'training_set': Port([]. self.run)}
 
   def run(self):
     records = self.input_port.get()
     records = [re.sub(RemoveUrls.URL_REGEX, "URL", record) for record in records]
     self.output_port.update(records)
 
-  def get_output_ports(self):
-    return [self.output_port]
+  def get_output_ports(self, port):
+    return self.output_ports[port]
 
 class NaiveBayes_model:
   def __init__(self, training_set, labels):
     self.training_set = training_set
     self.labels = labels
-    self.model = Port([], self.run)
+    self.output_ports = {'model': Port([], self.run)}
 
   def run(self):
     training_set = self.training_set.get()
@@ -35,22 +35,22 @@ class NaiveBayes_model:
     self.model.update(nltk.NaiveBayesClassifier.train( zip(training_set,
         labels)))
 
-  def get_output_ports(self):
-    return [self.model]
+  def get_output_ports(self, port):
+    return self.output_ports[port]
 
 class NaiveBayes_classifier:
   def __init__(self, model, data):
     self.model = model
     self.data = data
-    self.labels = Port([], self.run)
+    self.output_ports = {'labels': Port([], self.run)}
 
     def run(self):
       model = self.model.get()
       records = self.data.get()
       self.labels.update([model.classify(record) for record in records])
 
-    def get_output_ports(self):
-      return [self.labels]
+    def get_output_ports(self, port):
+      return self.output_ports[port]
 
 class Summarizer:
   """
@@ -63,8 +63,8 @@ class Summarizer:
   def __init__(self, records_port, labels_port, unigram_count=150):
     self.records_port = records_port
     self.labels_port = labels_port
-    self.tokens_port = Port([], self.run)
     self.unigram_count = unigram_count
+    self.output_ports = {'bag-of-words': Port([], self.run)}
 
   @run_once
   def run(self):
@@ -75,14 +75,14 @@ class Summarizer:
     self.tokens_port.update([word for word, count in \
         dist.most_common(self.unigram_count)])
 
-  def get_output_ports(self):
-    return [self.tokens_port]
+  def get_output_ports(self, port):
+    return self.output_ports[port]
 
 class evaluater:
   def __init__(self, labels, golden):
     self.labels = labels
     self.golden = golden
-    self.acc = Port([], self.run)
+    self.output_ports = {'accuracy': Port([], self.run)}
 
   @run_once
   def run(self):
@@ -94,8 +94,30 @@ class evaluater:
     else:
       self.acc.update(0)
 
-  def get_output_ports(self):
-    return [self.acc]
+  def get_output_ports(self, port):
+    return self.output_ports[port]
+
+class Reader:
+
+  def __init__(self, input_file_path):
+    self.input_file_path = input_file_path
+    self.output_ports = {'records': Port([], self.read), 'labels': Port([].
+      self.read)}
+
+  @run_once
+  def read(self):
+    sents = []
+    labels = []
+    csv_file = open(self.input_file_path, 'rb')
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+      sents += [row[0]]
+      labels += [row[1]]
+    self.sents.update(sents)
+    self.labels.update(labels)
+
+  def get_output_ports(self, port):
+    return self.output_ports[port]
 
 class Port:
   # TODO Update the active set of flags
@@ -114,26 +136,4 @@ class Port:
 
   def update(self, data):
     self.data = data
-
-class Reader:
-
-  def __init__(self, input_file_path):
-    self.input_file_path = input_file_path
-    self.sents = Port([], self.read)
-    self.labels = Port([], self.read)
-
-  def read(self):
-    sents = []
-    labels = []
-    csv_file = open(self.input_file_path, 'rb')
-    csv_reader = csv.reader(csv_file)
-    for row in csv_reader:
-      sents += [row[0]]
-      labels += [row[1]]
-    self.sents.update(sents)
-    self.labels.update(labels)
-
-  def get_output_ports(self):
-    return [self.sents, self.labels]
-
 
